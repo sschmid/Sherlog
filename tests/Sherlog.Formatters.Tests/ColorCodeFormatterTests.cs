0a95b09693396c40cb108620e5697755dd9d1d16
Simon Schmid <mail@sschmid.com>
2022-11-14 18:12:40 +0100

    Add formatter tests

---
+++ tests/Sherlog.Formatters.Tests/ColorCodeFormatterTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Xunit;
+using static Sherlog.Formatters.ColorCodeFormatter;
+
+namespace Sherlog.Formatters.Tests
+{
+    public class ColorCodeFormatterTests
+    {
+        readonly ColorCodeFormatter _formatter;
+        readonly Logger _logger;
+
+        public ColorCodeFormatterTests()
+        {
+            _formatter = new ColorCodeFormatter();
+            _logger = new Logger("TestLogger");
+        }
+
+        [Fact]
+        public void FormatsStringUsingTraceColors() => _formatter.FormatMessage(_logger, LogLevel.Trace, "test message")
+            .Should().Be($"{Esc}{CyanBackground}{Esc}{WhiteForeground}test message{Esc}{Reset}");
+
+        [Fact]
+        public void FormatsStringUsingDebugColors() => _formatter.FormatMessage(_logger, LogLevel.Debug, "test message")
+            .Should().Be($"{Esc}{NoBackground}{Esc}{BlueForeground}test message{Esc}{Reset}");
+
+        [Fact]
+        public void FormatsStringUsingInfoColors() => _formatter.FormatMessage(_logger, LogLevel.Info, "test message")
+            .Should().Be($"{Esc}{NoBackground}{Esc}{GreenForeground}test message{Esc}{Reset}");
+
+        [Fact]
+        public void FormatsStringUsingWarnColors() => _formatter.FormatMessage(_logger, LogLevel.Warn, "test message")
+            .Should().Be($"{Esc}{NoBackground}{Esc}{YellowForeground}test message{Esc}{Reset}");
+
+        [Fact]
+        public void FormatsStringUsingErrorColors() => _formatter.FormatMessage(_logger, LogLevel.Error, "test message")
+            .Should().Be($"{Esc}{RedBackground}{Esc}{WhiteForeground}test message{Esc}{Reset}");
+
+        [Fact]
+        public void FormatsStringUsingFatalColors() => _formatter.FormatMessage(_logger, LogLevel.Fatal, "test message")
+            .Should().Be($"{Esc}{MagentaBackground}{Esc}{WhiteForeground}test message{Esc}{Reset}");
+    }
+}
--- tests/Sherlog.Formatters.Tests/LogMessageFormatterTests.cs
+++ tests/Sherlog.Formatters.Tests/LogMessageFormatterTests.cs
@@ -5,20 +5,9 @@ namespace Sherlog.Formatters.Tests
 {
     public class LogMessageFormatterTests
     {
-        readonly LogMessageFormatter _formatter;
-        readonly Logger _logger;
-
-        public LogMessageFormatterTests()
-        {
-            _formatter = new LogMessageFormatter("[{1}] {0}: {2}");
-            _logger = new Logger("TestLogger");
-        }
-
         [Fact]
-        public void FormatsString()
-        {
-            _formatter.FormatMessage(_logger, LogLevel.Debug, "test message")
-                .Should().Be("[Debug] TestLogger: test message");
-        }
+        public void FormatsString() => new LogMessageFormatter("[{1}] {0}: {2}")
+            .FormatMessage(new Logger("TestLogger"), LogLevel.Debug, "test message")
+            .Should().Be("[Debug] TestLogger: test message");
     }
 }
